4106aa408f865ece0409ea27bee4e43fe8639b6d
1fbr <1fbr@proton.me>
2023-01-02 22:35:37 +0000

    fix: update products count

--- src/js/utils/pageNumber.js
+++ src/js/utils/pageNumber.js
@@ -1,7 +1,12 @@
-const TOTAL_PRODUCTS = 474
-const GADIS_PRODUCTS = 350
-const MERCADONA_PRODUCTS = 124
+const TOTAL_PRODUCTS = 866
+const GADIS_PRODUCTS = 352
+const MERCADONA_PRODUCTS = 297
+const FAMILIA_PRODUCTS = 96
+const FROIZ_PRODUCTS = 121
+
 export const limit = 60
 export const maxPages = Math.ceil(TOTAL_PRODUCTS / limit)
 export const gadisPages = Math.ceil(GADIS_PRODUCTS / limit)
 export const mercadonaPages = Math.ceil(MERCADONA_PRODUCTS / limit)
+export const familiaPages = Math.ceil(FAMILIA_PRODUCTS / limit)
+export const froizPages = Math.ceil(FROIZ_PRODUCTS / limit)
