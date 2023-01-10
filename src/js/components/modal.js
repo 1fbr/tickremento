15d86f4386e893dd3f5e1cf8fe9d869c6ec4151e
1fbr <1fbr@proton.me>
2023-01-10 19:24:20 +0000

    fix: remove non-functional button

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -6,16 +6,6 @@ modalHeader.classList.add('modal-header')
 const modalWindow = document.createElement('div')
 modalWindow.classList.add('modal-window')
 
-const actions = document.createElement('div')
-actions.classList.add('actions')
-
-const modalCloseButton = document.createElement('button')
-modalCloseButton.classList.add('modal-close-button')
-modalCloseButton.innerText = 'Aceptar'
-modalCloseButton.addEventListener('click', () => {
-  modalWindow.remove()
-})
-
 const modalContent = document.createElement('p')
 modalContent.classList.add('modal-content')
 
@@ -24,7 +14,5 @@ export const createModal = (header, message) => {
   modalContent.innerText = message
   modalWindow.appendChild(modalHeader)
   modalWindow.appendChild(modalContent)
-  actions.appendChild(modalCloseButton)
-  modalWindow.appendChild(actions)
   mainContainer.appendChild(modalWindow)
 }
