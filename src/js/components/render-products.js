dd29a79a1e2b4e7056e6b7e685c9cd9046ba6c70
1fbr <1fbr@proton.me>
2023-03-08 17:35:03 +0000

    feat: add a window with the search result

--- src/js/components/render-products.js
+++ src/js/components/render-products.js
@@ -1,9 +1,27 @@
 import { createCard } from './product-card.js'
 
+const mainContainer = document.querySelector('.main-container')
+
 const renderCards = (data, className) => {
   const container = document.createElement('section')
   container.classList.add(className)
   data.forEach(product => { createCard(product, container) })
+}
+
+const renderSearchCards = (data, className) => {
+  const searchWindow = document.createElement('div')
+  searchWindow.classList.add('search-window')
+
+  const output = document.createElement('p')
+  output.textContent = `↓ ${data.output} ↓`
+  output.id = 'search-output'
+
+  searchWindow.appendChild(output)
+  mainContainer.appendChild(searchWindow)
+
+  const container = document.createElement('section')
+  container.classList.add(className)
+  data.products.forEach(product => { createCard(product, container) })
 }
 
 export const renderProducts = (data) => {
@@ -11,7 +29,7 @@ export const renderProducts = (data) => {
 }
 
 export const renderSearchResults = (data) => {
-  renderCards(data, 'products-container')
+  renderSearchCards(data, 'products-container')
 }
 
 export const renderSupermarketProducts = (data) => {
