bbe04ba3a7ee863ef32a1c2f5e2fdc4df5ddc321
1fbr <1fbr@proton.me>
2023-03-11 15:35:50 +0000

    refactor: set price color and info

--- src/js/components/product-card.js
+++ src/js/components/product-card.js
@@ -1,67 +1,76 @@
-import { getPrices } from '../handlers/price-color-handler.js'
+import { getPriceColor } from '../handlers/price-color-handler.js'
 import { chartConfig } from '../utils/chart.js'
 
 const main = document.querySelector('.main-container')
 const fragment = document.createDocumentFragment()
 
 export const createCard = (product, productsContainer) => {
-  const supermarketName = `${product.supermarket.toLowerCase()}`
+  const { supermarket, url, img, productId, name, details, prices, months } = product
+
+  const supermarketVariable = `${supermarket.toLowerCase()}`
   const newCard = document.createElement('article')
-  newCard.classList.add(`${supermarketName}-card`)
-
+  newCard.classList.add(`${supermarketVariable}-card`)
   const productInfo = document.createElement('div')
-  productInfo.classList.add(`${supermarketName}-product-info`)
-
+  productInfo.classList.add(`${supermarketVariable}-product-info`)
   const innerInfo = document.createElement('div')
   innerInfo.classList.add('inner-info')
 
   const linkToProduct = document.createElement('a')
-  linkToProduct.href = product.url
+  linkToProduct.href = url
   linkToProduct.target = '_blank'
-
   const productImage = document.createElement('img')
-  productImage.src = product.img
-  productImage.alt = `Imagen del producto ${product.name}`
+  productImage.src = img
+  productImage.alt = `Imagen del producto ${name}`
   productImage.loading = 'lazy'
-  productImage.id = product.productId
+  productImage.id = productId
 
-  const supermarket = document.createElement('span')
-  supermarket.textContent = product.supermarket
-  supermarket.id = 'supermarket-name'
-
+  const supermarketName = document.createElement('span')
+  supermarketName.textContent = supermarket
+  supermarketName.id = 'supermarket-name'
   const productName = document.createElement('span')
-  productName.textContent = product.name
+  productName.textContent = name
   productName.id = 'product-name'
-
   const productDetails = document.createElement('small')
-  productDetails.textContent = product.details
+  productDetails.textContent = details
 
   const priceInfo = document.createElement('div')
   priceInfo.classList.add('price-info')
-
-  const prices = product.prices
-  const actualPrice = document.createElement('span')
-  const priceVariation = document.createElement('span')
-  getPrices(prices, actualPrice, priceVariation)
+  const currentPrice = document.createElement('span')
+  currentPrice.id = 'current-price'
+  const increase = document.createElement('span')
+  increase.id = 'increase'
+  const firstPrice = prices[0]
+  const lastPrice = prices[prices.length - 1]
+  const totalIncrease = (lastPrice - firstPrice).toFixed(2)
+  const percentageIncrease = (((lastPrice - firstPrice) / firstPrice) * 100).toFixed(2)
+  const totalIncreaseWithSign = (totalIncrease > 0 ? '+' : '') + totalIncrease
+  const percentageIncreaseWithSign = (percentageIncrease > 0 ? '+' : '') + percentageIncrease
+  currentPrice.textContent = convertToOutput(lastPrice, '€')
+  increase.textContent = `${convertToOutput(totalIncreaseWithSign, '€')} (${convertToOutput(percentageIncreaseWithSign, '%')})`
+  getPriceColor(totalIncrease, currentPrice, increase)
 
   const priceChart = document.createElement('canvas')
   priceChart.id = 'product-chart'
-  const monthsArray = product.months
-  const pricesArray = product.prices
+  const monthsArray = months
+  const pricesArray = prices
   chartConfig(monthsArray, pricesArray, priceChart)
 
   linkToProduct.appendChild(productImage)
   productInfo.appendChild(linkToProduct)
-  innerInfo.appendChild(supermarket)
+  innerInfo.appendChild(supermarketName)
   innerInfo.appendChild(productName)
   innerInfo.appendChild(productDetails)
   productInfo.appendChild(innerInfo)
-  priceInfo.appendChild(actualPrice)
-  priceInfo.appendChild(priceVariation)
+  priceInfo.appendChild(currentPrice)
+  priceInfo.appendChild(increase)
   newCard.appendChild(productInfo)
   newCard.appendChild(priceInfo)
   newCard.appendChild(priceChart)
   fragment.appendChild(newCard)
   productsContainer.appendChild(fragment)
   main.appendChild(productsContainer)
+}
+
+const convertToOutput = (variable, symbol) => {
+  return `${variable.replace('.', ',')}${symbol}`
 }
--- src/js/handlers/price-color-handler.js
+++ src/js/handlers/price-color-handler.js
@@ -1,5 +1,5 @@
-export let bgColor = 'rgb(255, 99, 132)'
-export let borderColor = 'rgb(255, 0, 0)'
+export let bgColor = ''
+export let borderColor = ''
 
 const setColor = (elements, color) => {
   elements.forEach(element => {
@@ -7,32 +7,15 @@ const setColor = (elements, color) => {
   })
 }
 
-export const getPrices = (prices, actualPrice, priceVariation) => {
-  const priceIncrement = (prices[prices.length - 1] - prices[0]).toFixed(2)
-  const porcentualIncrement = (((prices[prices.length - 1] - prices[0]) / prices[0]) * 100).toFixed(2)
-
-  actualPrice.id = 'actual-price'
-  actualPrice.textContent = `${prices[prices.length - 1].replace('.', ',')}€`
-
-  priceVariation.id = 'increment'
-  priceVariation.textContent = `+${priceIncrement.replace('.', ',')}€ (+${porcentualIncrement.replace('.', ',')}%)`
-
-  if (priceIncrement < 0) {
-    priceVariation.textContent = `${priceIncrement.replace('.', ',')}€ (${porcentualIncrement.replace('.', ',')}%)`
-    setColor([actualPrice, priceVariation], 'green')
-  } else if (priceIncrement == 0) { //eslint-disable-line
-    setColor([actualPrice, priceVariation], 'gray')
+export const getPriceColor = (totalIncrease, currentPrice, increase) => {
+  if (totalIncrease < 0) {
+    setColor([currentPrice, increase], 'green')
+    bgColor = borderColor = 'green'
+  } else if (totalIncrease == 0) { //eslint-disable-line
+    setColor([currentPrice, increase], 'gray')
+    bgColor = borderColor = 'gray'
   } else {
-    setColor([actualPrice, priceVariation], 'red')
+    setColor([currentPrice, increase], 'red')
+    bgColor = borderColor = 'red'
   }
-
-  defineChartColor(priceIncrement)
-}
-
-const defineChartColor = (priceIncrement) => {
-  bgColor = borderColor = priceIncrement < 0
-    ? 'rgb(0, 128, 0)'
-    : priceIncrement == 0 //eslint-disable-line
-      ? 'rgb(139,136,136)'
-      : 'rgb(255, 0, 0)'
 }
